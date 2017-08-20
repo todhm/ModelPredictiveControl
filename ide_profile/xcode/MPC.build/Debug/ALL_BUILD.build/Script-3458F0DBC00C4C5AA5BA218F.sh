1bcba1537545e37e48fd69bdc09194a720cc65be
강희명 <hm@ganghuimyeongs-MacBook-Pro.local>
2017-08-20 23:10:11 +0900

    submit for addition

---
+++ ide_profile/xcode/MPC.build/Debug/ALL_BUILD.build/Script-3458F0DBC00C4C5AA5BA218F.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/hm/desktop/homemaster/week14/term2/project5/CarND-MPC-Project/ide_profile/xcode -f /Users/hm/desktop/homemaster/week14/term2/project5/CarND-MPC-Project/ide_profile/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ ide_profile/xcode/MPC.build/Debug/ZERO_CHECK.build/Script-A83D86FD4C6B4E6E88E201F2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/hm/desktop/homemaster/week14/term2/project5/CarND-MPC-Project/ide_profile/xcode -f /Users/hm/desktop/homemaster/week14/term2/project5/CarND-MPC-Project/ide_profile/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
